c9447c6e9b2d1a57ec4e783c3b5b05629148a46d
Federico Spadaro <federicospadaro2@gmail.com>
2025-05-24 15:01:35 +0200

    milestone 2

--- politicians-app/src/App.jsx
+++ politicians-app/src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo} from 'react';
 import './App.css';
 
 function App() {
   const [politicians, setPoliticians] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:3333/politicians')
@@ -15,15 +16,30 @@ function App() {
       });
   }, []);
 
+  const filteredPolitic = useMemo(() => {
+    return politicians.filter(p => (
+      p.name.toLowerCase().includes(search.toLowerCase()) ||
+      p.biography.toLowerCase().includes(search.toLowerCase()) ||
+      p.position.toLowerCase().includes(search.toLowerCase)
+    ));
+  } , [politicians, search]);
+
   return (
     <div>
       <h1>Lista Politici</h1>
       <div className="politicians-container">
-        {politicians.map((politician) => {
+        <input
+          type="text"
+          placeholder="Cerca un politico..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        {filteredPolitic.map((politician) => {
           console.log(politician.image); // 👀 Per debug
 
           return (
             <div className="politicCard" key={politician.id}>
+              
               <img
                 src={politician.image.startsWith('http') ? politician.image : 'https://via.placeholder.com/150'}
                 alt={politician.name}
